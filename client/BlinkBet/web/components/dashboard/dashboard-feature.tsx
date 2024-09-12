dac04896cbeb189a73b844ada283825639bac82e
jpick713 <54317750+jpick713@users.noreply.github.com>
2024-09-12 00:41:54 -0400

    Update dashboard-feature.tsx

--- client/BlinkBet/web/components/dashboard/dashboard-feature.tsx
+++ client/BlinkBet/web/components/dashboard/dashboard-feature.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import { AppHero } from '../ui/ui-layout';
+import { QueryClient, useQuery } from '@tanstack/react-query';
 
 const links: { label: string; href: string }[] = [
   { label: 'Solana Docs', href: 'https://docs.solana.com/' },
@@ -14,11 +15,25 @@ const links: { label: string; href: string }[] = [
 ];
 
 export default function DashboardFeature() {
+  const { isLoading, error, data } = useQuery({
+    queryKey: ['action'],
+    queryFn: () =>
+      fetch('http://localhost:5001/price/action').then(
+        (res) => res.json()
+      ),
+  });
+
+  const solanaPrice = data?.solanaPrice;
+  const premium120 = data?.premium120;
+  const premium200 = data?.premium200;
+  const finalPremium = data?.finalPremium;
+
   return (
     <div>
       <AppHero title="gm" subtitle="Say hi to your new Solana dApp." />
       <div className="max-w-xl mx-auto py-6 sm:px-6 lg:px-8 text-center">
         <div className="space-y-2">
+          {!isLoading && <div>{Math.round(finalPremium * 1000) / 1000} %</div>}
           <p>Here are some helpful links to get you started.</p>
           {links.map((link, index) => (
             <div key={index}>
