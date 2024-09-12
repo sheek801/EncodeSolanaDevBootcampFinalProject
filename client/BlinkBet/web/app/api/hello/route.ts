dfdbf3d64fa1907fbe26e9f44e0f2509ee7fc81f
Risheek Somu <risheeksomu@risheeks-air.mynetworksettings.com>
2024-09-11 22:51:07 -0400

    done

--- client/BlinkBet/web/app/api/hello/route.ts
+++ client/BlinkBet/web/app/api/hello/route.ts
@@ -1,3 +1,15 @@
+import {ActionGetResponse} from "@solana/actions"
+
 export async function GET(request: Request) {
+  const response : ActionGetResponse = {
+    icon: 
+    description: "Select an SPL token and predict how much it will increase",
+    title: "BetBlink",
+    label: "10"
+    error:
+
+
+
+  }
   return new Response('Hello, from API!');
 }
